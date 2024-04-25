d5a91cf64f3d8607751578e3fe557be07de042a6
Thinley <thinleylama44@gmail.com>
2024-04-25 16:53:00 +0530

    No hydration in nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,18 +1,11 @@
 'use client'
 import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import MenuIcon from '@mui/icons-material/Menu';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import Logo from './assets/ytlogo.png'
 import Image from 'next/image';
 import SearchIcon from '@mui/icons-material/Search';
@@ -21,25 +14,6 @@ import VideoCallIcon from '@mui/icons-material/VideoCall';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 
 const drawerWidth = 200;
-
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
-  open?: boolean;
-}>(({ theme, open }) => ({
-  flexGrow: 1,
-  padding: theme.spacing(3),
-  transition: theme.transitions.create('margin', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  marginLeft: `-${drawerWidth}px`,
-  ...(open && {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  }),
-}));
 
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
@@ -51,7 +25,6 @@ const DrawerHeader = styled('div')(({ theme }) => ({
 }));
 
 export default function PersistentDrawerLeft() {
-  const theme = useTheme();
   const [open, setOpen] = React.useState(false);
 
   const handleDrawerOpen = () => {
@@ -111,77 +84,58 @@ export default function PersistentDrawerLeft() {
         open={open}
       >
         <div className='h-10 flex items-center'>
-          <button onClick={handleDrawerClose}>
-            <div className='flex gap-2'>
-              <div>
-                <button className='h-0' onClick={handleDrawerOpen}>
-                  <MenuIcon />
-                </button>
-              </div>
-              <div className='w-24'>
-                <Image src={Logo} alt='image' />
-              </div>
+          <div className='flex gap-2'>
+            <div>
+              <button className='h-0' onClick={handleDrawerClose}>
+                <MenuIcon />
+              </button>
             </div>
-          </button>
+            <div className='w-24'>
+              <Image src={Logo} alt='image' />
+            </div>
+          </div>
         </div>
 
         <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
+
+        <button>hi</button>
+        <button>hi</button>
+        <button>hi</button>
+        <button>hi</button>
+
       </Drawer>
-      <Main className='mx-2' open={open}>
+      
         <DrawerHeader />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-      </Main>
+        <div className='mt-16'>
+            <Typography paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+              enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+              imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+              Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+              Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+              adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+              nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+              leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+              feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+              consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+              sapien faucibus et molestie ac.
+            </Typography>
+            <Typography paragraph>
+              Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
+              eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
+              neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
+              tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
+              sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
+              tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
+              gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+              et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
+              tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+              eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+              posuere sollicitudin aliquam ultrices sagittis orci a.
+            </Typography>
+        </div>
+
     </Box>
   );
 }
